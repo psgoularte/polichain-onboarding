cc5d98aec875f50ab228336b6aa736f5d2a62b84
psgoularte <sgoularte.pedro@gmail.com>
2025-06-17 00:31:06 -0300

    correção erros deploy

--- aula-2/app/api/items/[id]/route.ts
+++ aula-2/app/api/items/[id]/route.ts
@@ -5,7 +5,8 @@ export async function PUT(
   request: Request,
   { params }: { params: { id: string } }
 ) {
-  const id = Number(params.id);
+  const id = Number(await params.id);
+  const { name } = await request.json();
   const updated = await prisma.item.update({ where: { id }, data: { name } });
   return NextResponse.json(updated);
 }
@@ -14,7 +15,7 @@ export async function DELETE(
   request: Request,
   { params }: { params: { id: string } }
 ) {
-  const id = Number(params.id);
+  const id = Number(await params.id);
   await prisma.item.delete({ where: { id } });
   return NextResponse.json({ ok: true });
 }
